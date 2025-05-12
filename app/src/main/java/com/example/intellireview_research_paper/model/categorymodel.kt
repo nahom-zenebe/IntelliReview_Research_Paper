3e4f31443ca822d6cd151b23ab74be1737a601fb
Abel Tesfa <abeltesfa198@gmail.com>
2025-05-12 16:40:19 +0300

    category edit and delete functional

--- app/src/main/java/com/example/intellireview_research_paper/model/categorymodel.kt
+++ app/src/main/java/com/example/intellireview_research_paper/model/categorymodel.kt
@@ -1,10 +1,13 @@
 package com.example.intellireview_research_paper.model
 
+import com.google.gson.annotations.SerializedName
+
 data class categorymodel(
-    val categoryId:String?=null,
+    @SerializedName("_id")
+    val categoryId: String? = null,
+
     val name: String = "",
     val description: String = "",
     val createdAt: String? = null,
     val updatedAt: String? = null
-
 )
